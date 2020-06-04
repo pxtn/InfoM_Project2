752df49b0b50abe6275448f4b8cf0b31ec9098e2
Pascal Affentranger <pascal.a.14@gmail.com>
2020-06-04 22:29:02 +0200

    added custom queries for figures and orders

--- src/main/java/ch/zhaw/benchstore/repositories/FigureRepository.java
+++ src/main/java/ch/zhaw/benchstore/repositories/FigureRepository.java
@@ -2,7 +2,16 @@ package ch.zhaw.benchstore.repositories;
 
 import ch.zhaw.benchstore.entities.Figure;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FigureRepository extends JpaRepository<Figure, Long> {
+
+    @Query("SELECT fig FROM Figure fig WHERE fig.business.id = ?1")
+    public List<Figure> getFiguresForBusiness(long business_id);
+
+    @Query("SELECT fig FROM Figure fig WHERE fig.business.id = ?1 AND MONTH(date) = ?2 AND YEAR(date) = ?3")
+    public List<Figure> getFiguresForBusinessByMonthOfYear(long business_id, int year, int month);
 
 }
--- src/main/java/ch/zhaw/benchstore/repositories/OrderRepository.java
+++ src/main/java/ch/zhaw/benchstore/repositories/OrderRepository.java
@@ -2,7 +2,16 @@ package ch.zhaw.benchstore.repositories;
 
 import ch.zhaw.benchstore.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT ord FROM Order ord WHERE ord.customer.id = ?1")
+    public List<Order> getOrdersFromCustomerWithId(long id);
+
+    @Query("SELECT ord, DATEDIFF(date_of_delivery, date_of_order) AS deliveryTime FROM Order ord WHERE DATEDIFF(date_of_delivery, date_of_order) > 14")
+    public List<Order> getDelayedOrders();
 
 }
