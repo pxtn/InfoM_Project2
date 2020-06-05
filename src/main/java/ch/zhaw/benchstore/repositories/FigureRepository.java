6d93991e4ddeab76f8aa6067ec619d8e597728b7
Pascal Affentranger <pascal.a.14@gmail.com>
2020-06-05 13:53:22 +0200

    fixed bug in figure query

--- src/main/java/ch/zhaw/benchstore/repositories/FigureRepository.java
+++ src/main/java/ch/zhaw/benchstore/repositories/FigureRepository.java
@@ -11,7 +11,7 @@ public interface FigureRepository extends JpaRepository<Figure, Long> {
     @Query("SELECT fig FROM Figure fig WHERE fig.business.id = ?1")
     public List<Figure> getFiguresForBusiness(long business_id);
 
-    @Query("SELECT fig FROM Figure fig WHERE fig.business.id = ?1 AND MONTH(date) = ?2 AND YEAR(date) = ?3")
+    @Query("SELECT fig FROM Figure fig WHERE fig.business.id = ?1 AND YEAR(fig.date) = ?2 AND MONTH(fig.date) = ?3")
     public List<Figure> getFiguresForBusinessByMonthOfYear(long business_id, int year, int month);
 
 }
